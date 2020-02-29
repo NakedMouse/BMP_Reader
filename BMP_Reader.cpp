2c1683aa829c77f7c1ac3e2957ec660ccc47679f
NakedMouse <769901432@qq.com>
2020-02-29 23:20:22 +0800

    2.1

--- BMP_Reader.cpp
+++ BMP_Reader.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-#define BYTE unsigned char
 
 //struct存在以4或8为标志进行字节对齐，需要用pack（2）限定对齐系数大小
 struct FileHader {
@@ -29,11 +28,11 @@ struct InfoHader {
 }infoHader;
 
 struct ColorTable {
-	unsigned char* colorData;
+	unsigned char*	colorData;
 }clrTable;
 
 struct Image {
-	unsigned char* imageData;
+	unsigned char*	imageData;
 	unsigned long	width;
 	unsigned long	height;
 	unsigned int	channels;
@@ -55,23 +54,24 @@ void main()
 
 	//无colorTable情况
 	if (fileHader.bfOffBits == 54) {
-		int width = infoHader.biWidth;
-		int height = infoHader.biHeight;
-		if (infoHader.biBitCount == 24) {
-			if (dataBuf = (unsigned char*)malloc(width * height * 3 * sizeof(char))) {
-				memset(dataBuf, 0, width * height * 3 * sizeof(char));
-			}
-			else {
-				cout << "Malloc Error" << endl;
-				return;
-			}
-			imageDatas.channels = 3;
-			imageDatas.width = width;
-			imageDatas.height = height;
-			file.read((char*)dataBuf, (long)width * (long)height * 3 * sizeof(char));
-			imageDatas.imageData = dataBuf;
-			//cout << dataBuf[0] - NULL << endl;
+		unsigned int width = infoHader.biWidth;
+		unsigned int height = infoHader.biHeight;
+		unsigned int channels = infoHader.biBitCount / 8;
+		unsigned int length = width * height * channels;
+
+		if (dataBuf = (unsigned char*)malloc(length * sizeof(char))) {
+			memset(dataBuf, 0, length * sizeof(char));
 		}
+		else {
+			cout << "Malloc Error" << endl;
+			return;
+		}
+		imageDatas.channels = channels;
+		imageDatas.width = width;
+		imageDatas.height = height;
+		file.read((char*)dataBuf, length * sizeof(char));
+		imageDatas.imageData = dataBuf;
+		//cout << dataBuf[0] - NULL << endl;
 	}
 	else {
 
@@ -81,13 +81,10 @@ void main()
 	//输出文件
 	string fileOut = "resources//1.txt";
 	FILE* fout = fopen(fileOut.c_str(), "w+");
-	//fprintf(fout, "aa");
 	for (unsigned int i = 0; i <= (int)imageDatas.width * imageDatas.height * 3; i+=3) {
 		if (i % (500*3) == 0 && i!=0) fprintf(fout, "\n");
-		fprintf(fout, "(%X %X %X)   ", (imageDatas.imageData[i]), (imageDatas.imageData[i+1]), (imageDatas.imageData[i+2] ));
-		//fprintf(fout, "(%d)   ", (int)(imageDatas.imageData[i] - NULL));
+		fprintf(fout, "(%X %X %X)   ", imageDatas.imageData[i], imageDatas.imageData[i + 1], imageDatas.imageData[i + 2]);
 	}
-	//fprintf(fout, "(%d)   ", (int)(imageDatas.imageData[0] - NULL));
 	//fout << nNum << "," << str << std::endl;
 	//fprintf(fout, "%.3f  %.3f  %.6f\n", j * p.h, l * p.h, u[40][l][j]);
 	fclose(fout);
