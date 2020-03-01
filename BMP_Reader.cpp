7b8189a059921445022048ac67ea088a52437e81
NakedMouse <769901432@qq.com>
2020-03-01 10:41:03 +0800

    2.2
    finish gray histogram

--- BMP_Reader.cpp
+++ BMP_Reader.cpp
@@ -36,25 +36,17 @@ struct Image {
 	unsigned long	width;
 	unsigned long	height;
 	unsigned int	channels;
+	unsigned int	length;
 }imageDatas;
 
-void printData(string fileIn);
-void getGrayHistogram(string);
-void readBMP(string fileIn);
+void printData();
+void getGrayHistogram();
+void transToGray();
 
 void main()
 {
-	//string fileIn = "resources//lena_gray.bmp";
-	string fileIn = "resources//lena_24bit.bmp";
-	
-	printData(fileIn);
-}
-
-void readBMP(string fileIn) {
-
-}
-
-void printData(string fileIn) {
+	string fileIn = "resources//lena_gray.bmp";
+	//string fileIn = "resources//lena_24bit.bmp";
 	ifstream file(fileIn, ios::in | ios::binary);
 	if (file.fail()) {
 		cout << "File Error" << endl;
@@ -78,6 +70,7 @@ void printData(string fileIn) {
 	imageDatas.channels = channels;
 	imageDatas.width = width;
 	imageDatas.height = height;
+	imageDatas.length = length;
 
 	//无colorTable情况
 	if (fileHader.bfOffBits == 54) {
@@ -100,11 +93,19 @@ void printData(string fileIn) {
 		file.read((char*)dataBuf, length * sizeof(unsigned char));
 		imageDatas.imageData = dataBuf;
 	}
+	file.close();
+
+	//将数组打印到txt上
+	printData();
+	getGrayHistogram();
+}
 
 
+void printData() {
 	//输出文件
-	string fileOut = "resources//1.txt";
+	string fileOut = "resources//data.txt";
 	FILE* fout = fopen(fileOut.c_str(), "w+");
+	unsigned int length = imageDatas.length;
 	for (unsigned int i = 0; i < (int)length * sizeof(unsigned char); i++) {
 		//if (i % 2 == 0 && i != 0) fprintf(fout, "  ");
 		fprintf(fout, "%3.X ", imageDatas.imageData[i]);
@@ -112,32 +113,36 @@ void printData(string fileIn) {
 	//fout << nNum << "," << str << std::endl;
 	//fprintf(fout, "%.3f  %.3f  %.6f\n", j * p.h, l * p.h, u[40][l][j]);
 	fclose(fout);
-	file.close();
 }
 
-void getGrayHistogram(string fileIn) {
-	ifstream file(fileIn, ios::binary);
-	if (file.fail()) {
-		cout << "File Open Error" << endl;
+//计算灰度直方图并打印到GrayHistogram.txt
+void getGrayHistogram() {
+	if (imageDatas.channels != 1) {
+		cout << "Not Gray BMP , Can Not Get Gray Histogram" << endl;
 		return;
 	}
-	file.read((char*)&fileHader, 14);
-	file.read((char*)& infoHader, 40);
 
-	unsigned char* dataBuf = NULL;
-	unsigned int width = infoHader.biWidth;
-	unsigned int height = infoHader.biHeight;
-	unsigned int channels = infoHader.biBitCount / 8;
-	unsigned int length = width * height * channels;
-	if (dataBuf = (unsigned char*)malloc(length * sizeof(char))) {
-		memset(dataBuf, 0, length * sizeof(char));
+	unsigned int grayHistoGram[256] = { 0 };
+	
+	for (unsigned int i = 0; i < imageDatas.length; i++) {
+		int index = (int)(imageDatas.imageData[i] - NULL);
+		grayHistoGram[index]++;
 	}
-	else {
-		cout << "Malloc Error" << endl;
+
+	string fileOut = "resources//GrayHistogram.txt";
+	FILE* fout = fopen(fileOut.c_str(), "w+");
+	for (int i = 0; i < 256; i++) {
+		//if (i % 10 == 0 && i != 0) fprintf(fout, "\n");
+		fprintf(fout, "%.3d,%.4d\n", i, grayHistoGram[i]);
+	}
+
+	fclose(fout);
+}
+
+//将24位真彩色转换为灰度图像并打印到txt
+void transToGray() {
+	if (imageDatas.channels != 3) {
+		cout << "Is Not 24Bit Image , Can not Transfer TO Gray Image" << endl;
 		return;
 	}
-	imageDatas.channels = channels;
-	imageDatas.width = width;
-	imageDatas.height = height;
-
 }
