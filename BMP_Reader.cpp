e81600b902a32fc8cb28169dd40822a1f8e6f920
NakedMouse <769901432@qq.com>
2020-03-01 15:38:56 +0800

    2.4
    consider extra byte

--- BMP_Reader.cpp
+++ BMP_Reader.cpp
@@ -2,6 +2,7 @@
 #pragma pack(2)
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 //struct存在以4或8为标志进行字节对齐，需要用pack（2）限定对齐系数大小
@@ -34,6 +35,7 @@ struct ColorTable {
 struct Image {
 	unsigned char*	imageData;
 	unsigned long	width;
+	unsigned long	l_width;
 	unsigned long	height;
 	unsigned int	channels;
 	unsigned int	length;
@@ -45,8 +47,8 @@ void transToGray();
 
 void main()
 {
-	//string fileIn = "resources//lena_gray.bmp";
-	string fileIn = "resources//lena_24bit.bmp";
+	//string fileIn = "resources//lena_low_8bit.bmp";
+	string fileIn = "resources//lena_low_24bit.bmp";
 	ifstream file(fileIn, ios::in | ios::binary);
 	if (file.fail()) {
 		cout << "File Error" << endl;
@@ -56,26 +58,25 @@ void main()
 	file.read((char*)& infoHader, 40);
 
 	unsigned char* dataBuf = NULL;
-	unsigned int width = infoHader.biWidth;
-	unsigned int height = infoHader.biHeight;
 	unsigned int channels = infoHader.biBitCount / 8;
-	unsigned int length = width * height * channels;
-	if (dataBuf = (unsigned char*)malloc(length * sizeof(char))) {
-		memset(dataBuf, 0, length * sizeof(char));
+	unsigned int length = infoHader.biSizeImage;
+	if (dataBuf = (unsigned char*)malloc(length)) {
+		memset(dataBuf, 0, length );
 	}
 	else {
 		cout << "Malloc Error" << endl;
 		return;
 	}
 	imageDatas.channels = channels;
-	imageDatas.width = width;
-	imageDatas.height = height;
-	imageDatas.length = length;
+	imageDatas.width = infoHader.biWidth;
+	imageDatas.l_width = (imageDatas.width * imageDatas.channels + 3) / 4 * 4;
+	imageDatas.height = infoHader.biHeight;
+	imageDatas.length = imageDatas.l_width * imageDatas.height;
 
 	//无colorTable情况
 	if (fileHader.bfOffBits == 54) {
 		file.seekg(fileHader.bfOffBits);
-		file.read((char*)dataBuf, length * sizeof(unsigned char));
+		file.read((char*)dataBuf, length );
 		imageDatas.imageData = dataBuf;
 		//cout << dataBuf[0] - NULL << endl;
 	}
@@ -89,8 +90,9 @@ void main()
 		}
 		file.read((char*)clrBuf, fileHader.bfOffBits - 54);
 		clrTable.colorData = clrBuf;
+
 		file.seekg(fileHader.bfOffBits);
-		file.read((char*)dataBuf, length * sizeof(unsigned char));
+		file.read((char*)dataBuf, length);
 		imageDatas.imageData = dataBuf;
 	}
 	file.close();
@@ -109,7 +111,7 @@ void printData() {
 	string fileOut = "resources//data.txt";
 	FILE* fout = fopen(fileOut.c_str(), "w+");
 	unsigned int length = imageDatas.length;
-	for (unsigned int i = 0; i < (int)length * sizeof(unsigned char); i++) {
+	for (unsigned int i = 0; i < (unsigned int)length; i++) {
 		//if (i % 2 == 0 && i != 0) fprintf(fout, "  ");
 		fprintf(fout, "%3.X ", imageDatas.imageData[i]);
 	}
@@ -149,38 +151,62 @@ void transToGray() {
 		return;
 	}
 
-	unsigned int length = imageDatas.width * imageDatas.height;
+	unsigned int length = imageDatas.length;
 	//Gray = (R*299 + G*587 + B*114 + 500) / 1000 转换算法
-	unsigned int* gray;
-	if (gray = (unsigned int*)malloc(length*sizeof(unsigned int))) {
-		memset(gray, 0, length * sizeof(unsigned int));
-	}
-	else {
-		cout << "TransToGray Malloc Error" << endl;
-		return;
+	vector<vector<unsigned int>> gray;
+
+	//for (int i = 0, j = 0, m = 0, n = 0; i < length; i++) {
+	//	unsigned int R = (int)(imageDatas.imageData[j] - NULL);
+	//	unsigned int G = (int)(imageDatas.imageData[j+1] - NULL);
+	//	unsigned int B = (int)(imageDatas.imageData[j+2] - NULL);
+	//	j += 3;
+	//	gray[i ][ j] = (R * 299 + G * 587 + B * 144 + 500) / 1000;
+	//	if()
+	//}
+
+	for (int i = 0; i < imageDatas.height; i++) {
+		vector<unsigned int> vec;
+		gray.push_back(vec);
 	}
 
-	for (int i = 0, j = 0; i < length; i++) {
-		unsigned int R = (int)(imageDatas.imageData[j] - NULL);
-		unsigned int G = (int)(imageDatas.imageData[j+1] - NULL);
-		unsigned int B = (int)(imageDatas.imageData[j+2] - NULL);
-		j += 3;
-		gray[i] = (R * 299 + G * 587 + B * 144 + 500) / 1000;
+	for (int h = 0, w = 0, i; h < imageDatas.height; h++) {
+		for (w = 0, i = 0; w < imageDatas.width; w++) {
+			unsigned int R = (unsigned int)imageDatas.imageData[h * imageDatas.l_width + i];
+			unsigned int G = (unsigned int)imageDatas.imageData[h * imageDatas.l_width + i + 1];
+			unsigned int B = (unsigned int)imageDatas.imageData[h * imageDatas.l_width + i + 2];
+			i += 3;
+			unsigned int temp;
+			temp = (R * 299 + G * 587 + B * 144 + 500) / 1000;
+			gray[h].push_back(temp);
+		}
 	}
 
 	string originOut = "resources\\TransOrigin.txt";
 	string transOut = "resources\\TransOut.txt";
 	FILE* fout = fopen(originOut.c_str(), "w+");
 	fprintf(fout, "Origin Image Datas \n");
-	for (int i = 0; i < length * 3; i++) {
-		fprintf(fout, "%3.X  ", imageDatas.imageData[i]);
+	//for (int i = 0; i < length; i++) {
+	//	fprintf(fout, "%3.X  ", imageDatas.imageData[i]);
+	//}
+	for (int h = 0; h < imageDatas.height; h++) {
+		for (int i = 0, w = 0; i < imageDatas.width; i++) {
+			unsigned int R = (unsigned int)imageDatas.imageData[h * imageDatas.l_width + w];
+			unsigned int G = (unsigned int)imageDatas.imageData[h * imageDatas.l_width + w + 1];
+			unsigned int B = (unsigned int)imageDatas.imageData[h * imageDatas.l_width + w + 2];
+			w += 3;
+			fprintf(fout, "(%2.2X %2.2X %2.2X) ", R, G, B);
+		}
+		fprintf(fout, "\n");
 	}
 
 	fclose(fout);
 	fout = fopen(transOut.c_str(), "w+");
 	fprintf(fout, "\n Gray Image Datas (unsigned int type)\n");
-	for (int i = 0; i < length; i++) {
-		fprintf(fout, "%.3d  ", gray[i]);
+	for (int i = 0; i < imageDatas.height; i++) {
+		for (int j = 0; j < imageDatas.width; j++) {
+			fprintf(fout, "%2.2X  ", gray[i][j]);
+		}
+		fprintf(fout, "\n");
 	}
 	fclose(fout);
 	return;
